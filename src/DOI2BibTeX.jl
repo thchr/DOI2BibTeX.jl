f958553012ea32d0cd8e7afc7d30e4cb20ff3ee5
Stefan Bringuier <stefanbringuier@gmail.com>
2024-01-29 11:06:48 -0800

    Add support for ArXiv preprints.

--- src/DOI2BibTeX.jl
+++ src/DOI2BibTeX.jl
@@ -1,9 +1,10 @@
 module DOI2BibTeX
 
-export doi2bib, journal_abbreviation
+export doi2bib, arxiv2bib, journal_abbreviation
 
 include("abbreviate.jl")
 include("query-doi.jl")
+include("query-arxiv.jl")
 include("precompile.jl")
 
 end
