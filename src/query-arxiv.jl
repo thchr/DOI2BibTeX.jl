d3fb739a65506c0e5e328f64ee872b9bb8f75cbe
Stefan Bringuier <stefanbringuier@gmail.com>
2024-01-29 12:04:32 -0800

    Add tests.

--- src/query-arxiv.jl
+++ src/query-arxiv.jl
@@ -2,7 +2,7 @@
 # ArXiv API https://info.arxiv.org/help/api/basics.html
 function _arxiv2bib(arxiv::AbstractString)
 	arxiv = _replace(arxiv,
-		"arxiv:" => "", "http://" => "", "https://" => "", "arxiv.org/" => "", "arxiv.org/abs" => "")
+		"arxiv:" => "", "http://" => "", "https://" => "", "arxiv.org/" => "", "abs/" => "")
 	return String(HTTP.get("https://arxiv.org/bibtex/$arxiv",
 		["Accept" => "application/x-bibtex"]).body)::String
 end
@@ -12,7 +12,7 @@ function arxiv2bib(arxiv::AbstractString;
 	abbreviate::Bool = true,
 )
 	arxiv_bib = _arxiv2bib(arxiv)
-    # _prettify_bib doesn't completly catch arXiv bibtex frmt
-    arxiv_bib = replace(arxiv_bib, r"(?<! )=(?! )" => " = ")
+	# _prettify_bib doesn't completly catch arXiv bibtex frmt
+	arxiv_bib = replace(arxiv_bib, r"(?<! )=(?! )" => " = ")
 	return _prettify_bib(arxiv_bib, minimal, abbreviate)
 end
